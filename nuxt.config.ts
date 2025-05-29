e9bd36e0313fc7f8d481298306a041372d8b5d79
takashiraki <stgtbike4649@gmail.com>
2025-05-29 12:49:47 +0000

    shadcn setup.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook']
-})
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: true },
+    modules: ["@nuxtjs/storybook", "shadcn-nuxt"],
+    css: ["~/assets/css/tailwind.css"],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: "",
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: "./components/ui",
+    },
+});
